6468aeb10316e8158cef94984adaf2799ab9ea7d
iwanoko <32120010+iwanoko@users.noreply.github.com>
2017-12-04 10:58:34 +0900

    second commit

--- index.js
+++ index.js
@@ -4,7 +4,33 @@ $(function () {
             var elemPos = $(this).offset().top;
             var scroll = $(window).scrollTop();
             var windowHeight = $(window).height();
-            if (scroll > elemPos - windowHeight + windowHeight / 5) {
+            if (scroll > elemPos - windowHeight + windowHeight / 15) {
+                $(this).addClass('scrollin');
+            }
+        });
+    });
+});
+
+$(function () {
+    $(window).scroll(function () {
+        $('.fadein2').each(function () {
+            var elemPos = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > elemPos - windowHeight + windowHeight / 3) {
+                $(this).addClass('scrollin');
+            }
+        });
+    });
+});
+
+$(function () {
+    $(window).scroll(function () {
+        $('.fadein3').each(function () {
+            var elemPos = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > elemPos - windowHeight + windowHeight / 2) {
                 $(this).addClass('scrollin');
             }
         });
